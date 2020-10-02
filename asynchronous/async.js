08e3efd17588a377dd0c56b085f3291faca49bc0
ThuhtetCoder <59603198+ThuhtetCoder@users.noreply.github.com>
2020-10-03 00:09:36 +0630

    asynchronous02

--- asynchronous/async.js
+++ asynchronous/async.js
@@ -1,14 +1,39 @@
 console.log('%c ajax request','color: green; font-size: 20px;');
 
-// window.onload = function(){
-//     var http = new XMLHttpRequest();
+    // ! Ajax Request
+    function getData(){
+        return new Promise((resolve,reject)=>{
+            var http = new XMLHttpRequest();
+            http.onreadystatechange = function(){
+                if(http.readyState == 4 && http.status == 200){
+                    const data = JSON.parse(http.response);
+                    resolve(data);
+                }else if(http.readyState == 4){
+                    reject('get error');
+                }
+            }
+            http.open("GET","https://jsonplaceholder.typicode.com/posts/",true);
+            http.send();
+        });
+    }
+    getData().then(response => {
+        console.log(response);
+        console.log('hello after getting data')
+    }).catch(err => {
+        console.log(err)
+    })
+    //end
 
-//     http.onreadystatechange = function(){
-//         if(http.readyState == 4 && http.status == 200){
-//             console.log(http);
-//         }   
-//     }
-//     http.open("GET","tweet.json",true);
-//     http.send();
-// };
+    // ! Fetch API (return promise)
+    fetch("https://jsonplaceholder.typicode.com/posts/").then(res => {
+        return res.json(); // this will return a Promise
+    }).then(data => {
+        console.log('resolved: ',data);
+    }).catch(err => {   
+        console.log(err)
+    });
+   
+    
+
+
 
